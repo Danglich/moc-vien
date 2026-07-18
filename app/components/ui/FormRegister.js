4c07e8b34a0234ea7a4f35ae7ff7cfc9be4fe66d
Danglich <86116972+Danglich@users.noreply.github.com>
2026-07-18 15:17:07 +0700

    edit

--- app/components/ui/FormRegister.js
+++ app/components/ui/FormRegister.js
@@ -21,92 +21,23 @@ export default function FormRegister() {
   };
 
   return (
-    <div className="max-w-5xl mx-auto bg-primary border border-orange-200 rounded-lg p-5 md:p-6 md:text-[17px] mt-8">
-      <h2 className="text-xl md:text-2xl font-semibold mb-4">
-        Yêu cầu tư vấn
-      </h2>
+    <section className="contact-section">
+          <div className="container contact-box">
+            <div className="contact-content">
+              <span>Tư vấn thiết kế miễn phí</span>
 
-      <p className="text-sm md:text-base text-gray-600 mb-6 ">
-        *Vui lòng để lại thông tin, nhân viên tư vấn sẽ liên hệ lại quý khách trong thời gian sớm nhất
-      </p>
+              <h2>Bạn đang chuẩn bị xây nhà?</h2>
 
-      <form
-        onSubmit={handleSubmit}
-        className="grid grid-cols-1 md:grid-cols-2 gap-6"
-      >
-        {/* Họ tên */}
-        <div>
-          <label className="block mb-2 text-sm md:text-base font-medium">
-            Họ và tên
-          </label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Họ tên"
-            value={form.name}
-            onChange={handleChange}
-            className="w-full rounded-md border border-gray-300 px-4 py-2 bg-white focus:outline-none focus:ring-2 focus:ring-red-400"
-          />
-        </div>
+              <p>
+                Liên hệ Mộc Viên để được tư vấn công năng, phong cách kiến trúc
+                và dự toán chi phí phù hợp.
+              </p>
+            </div>
 
-        {/* Bạn quan tâm */}
-        <div>
-          <label className="block mb-2 text-sm md:text-base font-medium">
-            Bạn quan tâm
-          </label>
-          <select
-            name="interest"
-            value={form.interest}
-            onChange={handleChange}
-            className="w-full rounded-md border border-gray-300 px-4 py-2 bg-white focus:outline-none focus:ring-2 focus:ring-red-400"
-          >
-            <option value="">Nhu cầu của bạn</option>
-            <option value="buy">Mua sản phẩm</option>
-            <option value="consult">Tư vấn</option>
-            <option value="support">Hỗ trợ</option>
-          </select>
-        </div>
-
-        {/* Số điện thoại */}
-        <div>
-          <label className="block mb-2 text-sm md:text-base font-medium">
-            Số điện thoại
-          </label>
-          <input
-            type="text"
-            name="phone"
-            placeholder="Điện thoại"
-            value={form.phone}
-            onChange={handleChange}
-            className="w-full rounded-md border border-gray-300 px-4 py-2 bg-white focus:outline-none focus:ring-2 focus:ring-red-400"
-          />
-        </div>
-
-        {/* Nội dung */}
-        <div>
-          <label className="block mb-2 text-sm md:text-base font-medium">
-            Nội dung
-          </label>
-          <textarea
-            name="message"
-            placeholder="Mô tả yêu cầu của bạn"
-            value={form.message}
-            onChange={handleChange}
-            rows={4}
-            className="w-full rounded-md border border-gray-300 px-4 py-2 bg-white focus:outline-none focus:ring-2 focus:ring-red-400"
-          />
-        </div>
-
-        {/* Button */}
-        <div className="md:col-span-2 flex justify-center mt-4 zoom-animation">
-          <button
-            type="submit"
-            className="bg-red-600 hover:bg-red-700 text-white font-medium px-6 py-2 rounded-md transition"
-          >
-            Yêu cầu tư vấn
-          </button>
-        </div>
-      </form>
-    </div>
+            <a href="tel:0354136968" className="contact-button">
+              Hotline: 035.413.6968
+            </a>
+          </div>
+        </section>
   );
 }
